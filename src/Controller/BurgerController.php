49585d640201873571a4db28545a028444b3c38c
Robin HALM <robin.halm68@gmail.com>
2024-09-11 08:03:18 +0200

    [refac] burger list as class atribute

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -7,8 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/burger', name: 'burger_')]
-class BurgerController extends AbstractController
-{
+class BurgerController extends AbstractController {
+    private array $burgers = [
+        '0' => ['name' => 'cheeseburger', 'description' => 'burger avec du fromage'],
+        '1' => ['name' => 'chikenCrusty', 'description' => 'burger au poulet croustillant']
+    ];
+
     #[Route('/liste', name: 'liste')]
     public function list(): Response {
         return $this->render('burgers_list.html.twig');
@@ -16,10 +20,9 @@ class BurgerController extends AbstractController
 
     #[Route('/{id}', name: 'details')]
     public function show(int $id): Response {
-        $burgers = [['id'=> '0', 'name' => 'cheeseburger', 'description' => 'burger avec du fromage'], ['id'=> '1', 'name' => 'chikenCrusty', 'description' => 'burger au poulet croustillant']];
+        
         try {
-            $burger = $burgers[$id];
-            
+            $burger = $this->burgers[$id] ?? null;
         } catch (\Exception $e) {
             $burger = null;
         }
