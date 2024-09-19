a981c5a8eac76ebeea8501360fd3d55466cbc8a5
Robin HALM <robin.halm68@gmail.com>
2024-09-19 08:12:10 +0200

    [feat] [exo] addition of form to create oignon

--- src/Controller/OignonController.php
+++ src/Controller/OignonController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Oignon;
+use App\Form\OignonType;
 use App\Repository\OignonRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/oignon', name: 'oignon_')]
 class OignonController extends AbstractController
 {
-    #[Route('/oignon', name: 'app_oignon')]
-    public function index(OignonRepository $oignonRepository): Response {
+    #[Route('/index', name: 'index')]
+    public function index(OignonRepository $oignonRepository): Response
+    {
         $oignons = $oignonRepository->findAll();
 
         return $this->render('oignon/index.html.twig', [
             'oignons' => $oignons,
+        ]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $oignon = new Oignon();
+        $form = $this->createForm(OignonType::class, $oignon);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($oignon);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('oignon_index');
+        }
+
+        return $this->render('oignon/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
