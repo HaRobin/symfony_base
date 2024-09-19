a89ff8161ce9d7572929ee9793bc7ebd15af6d49
Robin HALM <robin.halm68@gmail.com>
2024-09-19 09:19:31 +0200

    [config] stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,21 @@
 import './bootstrap.js';
+import './styles/app.css';
+import { Application } from 'stimulus';
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+ 
+// Créez une instance de l'application Stimulus
+const application = Application.start();
+ 
+// Importez les contrôleurs Stimulus
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
