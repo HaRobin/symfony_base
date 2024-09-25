d47ded68240bee04b995d8abaf573962a2ef8671
Robin HALM <robin.halm68@gmail.com>
2024-09-25 09:06:24 +0200

    [feat] exo5.1

---
+++ assets/controllers/sauce_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = ['sauce', 'selected'];
+
+    connect() {
+        console.log('Stimulus controller connected');
+    }
+
+    selectSauce(event) {
+        const sauceName = event.target.value;
+        console.log(`Selected sauce: ${sauceName}`);
+        this.selectedTarget.textContent = `Vous avez sélectionné : ${sauceName}`;
+    }
+}
